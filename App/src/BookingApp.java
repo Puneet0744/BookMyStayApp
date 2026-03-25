86da4b4229a6ec2ce7f08fe75c214cbab8198ce3
Puneet Khandelwal <puneetkh16@gmail.com>
2026-03-25 14:51:29 +0530

    Delete App directory

--- App/src/BookingApp.java
+++
@@ -1,8 +0,0 @@
-public class BookingApp {
-    public static void main(String[] args) {
-        // Print welcome message
-        System.out.println("Welcome to the Hotel Booking Management System!");
-
-        System.out.println("System initialized successfully.");
-    }
-}
