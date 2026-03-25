997a6b4cc275872eb7a63bd28886e7e2e860111d
Puneet0744 <puneetkh16@gmail.com>
2026-03-25 14:34:18 +0530

    Punnet added in branch UC1

---
+++ App/src/BookingApp.java
@@ -0,0 +1,8 @@
+public class BookingApp {
+    public static void main(String[] args) {
+        // Print welcome message
+        System.out.println("Welcome to the Hotel Booking Management System!");
+
+        System.out.println("System initialized successfully.");
+    }
+}
